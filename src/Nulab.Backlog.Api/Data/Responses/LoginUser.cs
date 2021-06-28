bb8e5f275660006b7d35e4bfd0fa799c254201df
石井　健太 <kenta.ishii@motex.co.jp>
2021-06-28 17:27:25 +0900

    LoginUser のファイルパスを調整した。

---
+++ src/Nulab.Backlog.Api/Data/Responses/LoginUser.cs
@@ -0,0 +1,30 @@
+﻿namespace Nulab.Backlog.Api.Data.Responses
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://developer.nulab.com/ja/docs/backlog/api/2/get-own-user/#
+    /// </remarks>
+    public sealed class LoginUser
+    {
+        public int id { get; set; }
+        
+        public string userId { get; set; }
+        
+        public string name { get; set; }
+        
+        public int roleType { get; set; }
+        
+        public string lang { get; set; }
+        
+        public string mailAddress { get; set; }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            return $"{nameof(id)}: {id}, {nameof(userId)}: {userId}, {nameof(name)}: {name}, {nameof(roleType)}: {roleType}, {nameof(lang)}: {lang}, {nameof(mailAddress)}: {mailAddress}";
+        }
+    }
+}
--- src/Nulab.Backlog.Api/IUsers.cs
+++ src/Nulab.Backlog.Api/IUsers.cs
@@ -24,26 +24,4 @@
         /// </remarks>
         Task<BacklogResponse<LoginUser>> GetMySelfAsync();
     }
-
-    public sealed class LoginUser
-    {
-        public int id { get; set; }
-        
-        public string userId { get; set; }
-        
-        public string name { get; set; }
-        
-        public int roleType { get; set; }
-        
-        public string lang { get; set; }
-        
-        public string mailAddress { get; set; }
-
-        /// <summary>Returns a string that represents the current object.</summary>
-        /// <returns>A string that represents the current object.</returns>
-        public override string ToString()
-        {
-            return $"{nameof(id)}: {id}, {nameof(userId)}: {userId}, {nameof(name)}: {name}, {nameof(roleType)}: {roleType}, {nameof(lang)}: {lang}, {nameof(mailAddress)}: {mailAddress}";
-        }
-    }
 }
