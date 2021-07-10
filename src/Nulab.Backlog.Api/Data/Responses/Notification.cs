156684ac58545a2ef6ae1ef3be43a40f77267c65
@IYEMON018 <iyemon018@gmail.com>
2021-07-10 22:51:49 +0900

    クラスを分割して適切な位置に再配置した。

---
+++ src/Nulab.Backlog.Api/Data/Responses/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace Nulab.Backlog.Api.Data.Responses
+{
+    public class Notification
+    {
+        public int id { get; set; }
+
+        public bool alreadyRead { get; set; }
+
+        public int reason { get; set; }
+
+        public User user { get; set; }
+
+        public bool resourceAlreadyRead { get; set; }
+    }
+}
---
+++ src/Nulab.Backlog.Api/Data/Responses/UserActivity.cs
@@ -0,0 +1,21 @@
+﻿namespace Nulab.Backlog.Api.Data.Responses
+{
+    using System;
+
+    public class UserActivity
+    {
+        public int id { get; set; }
+
+        public Project project { get; set; }
+
+        public int type { get; set; }
+
+        public Content content { get; set; }
+
+        public Notification[] notifications { get; set; }
+
+        public User createdUser { get; set; }
+
+        public DateTime created { get; set; }
+    }
+}
