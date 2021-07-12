bb4c80634ffc910e3e40bd82df57b55277f1c792
石井 健太 <i_m_a_walker@live.jp>
2021-07-12 14:58:09 +0900

    データクラスの位置が良くなかったので調整した。

---
+++ src/Nulab.Backlog.Api/Data/Responses/DiskUsage.cs
@@ -0,0 +1,29 @@
+﻿namespace Nulab.Backlog.Api.Data.Responses
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://developer.nulab.com/ja/docs/backlog/api/2/get-space-disk-usage/#%E3%83%AC%E3%82%B9%E3%83%9D%E3%83%B3%E3%82%B9%E3%83%9C%E3%83%87%E3%82%A3
+    /// </remarks>
+    public sealed class DiskUsage
+    {
+        public int capacity { get; set; }
+
+        public int issue { get; set; }
+        
+        public int wiki { get; set; }
+        
+        public int file { get; set; }
+        
+        public int subversion { get; set; }
+        
+        public int git { get; set; }
+        
+        public int gitLFS { get; set; }
+        
+        public int pullRequest { get; set; }
+        
+        public DiskUsageDetail[] details { get; set; }
+    }
+}
---
+++ src/Nulab.Backlog.Api/Data/Responses/DiskUsageDetail.cs
@@ -0,0 +1,21 @@
+﻿namespace Nulab.Backlog.Api.Data.Responses
+{
+    public class DiskUsageDetail
+    {
+        public int projectId { get; set; }
+
+        public int issue { get; set; }
+
+        public int wiki { get; set; }
+        
+        public int file { get; set; }
+        
+        public int subversion { get; set; }
+        
+        public int git { get; set; }
+        
+        public int gitLFS { get; set; }
+        
+        public int pullRequest { get; set; }
+    }
+}
